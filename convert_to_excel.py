fb31a88dfa6e0a87dcc5175e15dba444d6d6a9f5
Nithissh S <nithissh.s@freshworks.com>
2024-01-25 10:18:58 +0530

    Update convert_to_excel.py

--- convert_to_excel.py
+++ convert_to_excel.py
@@ -1,11 +1,29 @@
-import pandas as pd 
-import os 
- 
-frame = pd.DataFrame() 
-for filename in os.listdir(os.getcwd()): 
-    root, ext = os.path.splitext(filename) 
-    if ext == '.json': 
-        tmp_frame = pd.read_json(filename) 
-        frame = frame.append(tmp_frame, ignore_index=True) 
-         
-frame.to_csv('secret-scanner-results.csv', index=False) 
+# importing packages
+import pandas as pd
+
+# load json file using pandas
+df1 = pd.read_json('all-keys.json')
+
+# view data
+print(df1)
+
+# load json file using pandas
+df2 = pd.read_json('verified-keys.json')
+
+# view data
+print(df2)
+
+# use pandas.concat method 
+df = pd.concat([df1,df2])
+
+# view the concatenated dataframe
+print(df)
+
+# convert dataframe to csv file
+df.to_csv("secret-scanner-results.csv",index=False)
+
+# load the resultant csv file
+result = pd.read_csv("secret-scanner-results.csv")
+
+# and view the data
+print(result)
