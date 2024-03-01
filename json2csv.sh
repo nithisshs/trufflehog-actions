f1f130a475f40d859f2d15ab23a2e7e50b32a760
Nithissh S <nithissh.s@freshworks.com>
2024-03-01 15:01:34 +0530

    Update and rename json2csv.py to json2csv.sh

---
+++ json2csv.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Define input JSON file
+input_json="results.json"
+
+# Define output CSV file
+output_csv="secret-scan.csv"
+
+# Define headers
+headers="repo_name,link,commit,filename,Lines,Detector_type,verified_status,raw"
+
+# Extract relevant data using jq and save it to temporary JSON file
+jq -r '[
+    .SourceMetadata.Data.Github.repository,
+    .SourceMetadata.Data.Github.link,
+    .SourceMetadata.Data.Github.commit,
+    .SourceMetadata.Data.Github.file,
+    .SourceMetadata.Data.Github.line,
+    .DetectorName,
+    .Verified,
+    .Raw
+] | @csv' "$input_json" > temp.csv
+
+# Add headers to the output CSV file
+echo "$headers" > "$output_csv"
+cat temp.csv >> "$output_csv"
+
+# Remove temporary CSV file
+rm temp.csv
+
+echo "Conversion complete. Output saved to $output_csv"
